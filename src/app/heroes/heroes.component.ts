ad28ee3c53e115d3e0357319f18271e28025d7c5
tbduong <tiffaniduong@gmail.com>
2020-06-11 22:14:03 -0700

    Make hero name updateable.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 //import the hero interface from the hero.ts
 import { Hero } from '../hero';
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
