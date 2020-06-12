ef3abea79f5a4e8f38147c182380e231539917dc
tbduong <tiffaniduong@gmail.com>
2020-06-11 21:53:45 -0700

    create hero interface/schema and import to the component.html - render data

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+//Creates an interface or schema of an object
+//include key and also value TYPE
+export interface Hero {
+  id: number;
+  name: string;
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+//import the hero interface from the hero.ts
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+//Add code here
+//Creates Hero object
+hero: Hero = {
+   id: 1,
+   name: 'Windstorm'
+ };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
